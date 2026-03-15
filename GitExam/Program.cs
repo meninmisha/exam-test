0eccd8d1de9c4ae9c82aea74d34efcc9ca771ca0
M <meninmisha@gmail.com>
2026-03-15 10:30:11 +0200

    code  edited

--- GitExam/Program.cs
+++ GitExam/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+    Console.WriteLine("Enter the path to the folder:");
+    string folderPath = Console.ReadLine();
+
+    // Отримуємо файли 
+    string[] files = Directory.GetFiles(folderPath);
+
+    Console.WriteLine("File list:");
+
+    foreach (string file in files)
+    {
+        Console.WriteLine(file);
+    }
+
+    Console.WriteLine("Press any key to close");
+    Console.ReadKey();
